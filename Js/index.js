0b1c98300fa0eb1bc64d44287561caaeed7238ae
yashdobariya888@gmail.com <yashdoabriya888@gmail.com>
2021-07-08 18:48:32 +0530

    upto digital service section -done

---
+++ Js/index.js
@@ -0,0 +1,13 @@
+ 
+
+
+const openSideNav = () => {
+      document.getElementById("side-navbar").style.width = "100%";
+}
+const closeSideNav = () => {
+      window.event.preventDefault
+      document.getElementById("side-navbar").style.width = "0%";
+        
+}
+
+        
